e6a86880800506e8a19bbd2878dda42ce8a3860a
Eric Goodwin <edgbpc@mail.umsl.edu>
2018-04-21 12:07:52 -0500

    added page reference and type determination

--- user.c
+++ user.c
@@ -1,27 +1,29 @@
 #include "oss.h"
 
+#define read 1
+#define write 0
+
+
 unsigned int * simClock;
 Process * process;
 
 
+
 int main (int argc, char *argv[]){
-
-
 	printf("USER %d has launched\n", getpid());
 	
+	time_t timeSeed;
+	srand((int)time(&timeSeed) % getpid()); //%getpid used because children were all getting the same "random" time to run. 
 	int myPID = getpid();
 	int location = atoi(argv[1]);
 
 	int randomTerminateConstant = 90; //change this to increase or decrease chance process will terminate.  currently 50% to terminate
-	int randomRequestConstant = 50;
-	//seed random number generator
-	time_t timeSeed;
-	srand((int)time(&timeSeed) % getpid()); //%getpid used because children were all getting the same "random" time to run. 
-
-	//set number of pages this process has from 0 to 31	
-	int numPages = (rand() % 30) + 1;
-
+	int readFromMemoryChance = (rand() % 100) + 1; //sets precent chance memory reference will be a read.
+	int numPages = (rand() % 30) + 1; //set number of pages this process has from 0 to 31	
 	int index; //loop counter variable
+	int pageReference; //holds what page is being reference
+	int simulatedInvalidPageReferenceChance = (rand() % 100) + 1; //sets percent chance to cause a segmentation fault
+	int referenceType; //holds if reference type will be read or write
 
 	//signal handling
 	if (signal(SIGINT, handle) == SIG_ERR){
@@ -68,6 +70,26 @@ int main (int argc, char *argv[]){
 		//for (index = 0; index < numPages; index++){
 		//	printf("process %d has %d in page %d\n", getpid(), process[location].page[index], index ); //just want some kind of data in the page
 		//}
+	
+	if (simulatedInvalidPageReferenceChance > 99){ 
+		pageReference = numPages + 1;  //simulated seg fault.  change value in live above to increase odds.  currently 1%
+		printf("Process %d - Seg fault\n", myPID);
+	} else {
+		pageReference = (rand() % numPages);
+	}
+
+	printf("%d\n", readFromMemoryChance);
+
+	if (readFromMemoryChance > 50){
+		referenceType = read; //defined as 1
+	} else {
+		referenceType = write; //defined as 0
+	}
+	
+
+
+	printf("Process %d made a reference to %d page and type is %d\n",myPID, pageReference, referenceType);
+	
 
 sleep(5);
 
